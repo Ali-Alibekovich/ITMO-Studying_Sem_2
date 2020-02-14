4786dad3e632e18a8c32b8533adb0377933a101d
HusseinHalifaev <HusseinHalifaev@github.com>
2020-02-14 20:15:29 +0300

    .

--- Programming_Lab5/src/Main.java
+++ Programming_Lab5/src/Main.java
@@ -1,14 +1,30 @@
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Set<Users> users = new HashSet<>();
-        users.add(new Users(2,"Ali"));
-        users.add(new Users(2,"Ali"));
-        users.add(new Users(1,"Hussein"));
-        for (Users user:users){
-            System.out.println(user);
+        Scanner scan = new Scanner(System.in);
+        int n=0;
+        while (n!=3) {
+            System.out.println("1.Добавть пользователя");
+            System.out.println("2.Посмотреть список пользователей");
+            System.out.println("3.Выход");
+            n = scan.nextInt();
+            if (n == 1) {
+                int id;
+                String name;
+                System.out.println("Введите id и name (через пароль)");
+                id = scan.nextInt();
+                name = scan.next();
+                users.add(new Users(id, name));
+            }
+            if (n == 2) {
+                for (Users user : users) {
+                    System.out.println(user);
+                }
+            }
         }
-
     }
 }
--- Programming_Lab5/src/Users.java
+++ Programming_Lab5/src/Users.java
@@ -1,4 +1,5 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Users<T> {
     String name;
