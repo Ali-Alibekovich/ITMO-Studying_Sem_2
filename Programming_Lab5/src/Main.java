b2e7d01e183a18e6c433dc98096bc352cd4fc95c
HusseinHalifaev <HusseinHalifaev@github.com>
2020-02-16 13:12:57 +0300

    StartLab5

--- Programming_Lab5/src/Main.java
+++ Programming_Lab5/src/Main.java
@@ -3,7 +3,6 @@ import java.util.Hashtable;
 
 public class Main {
     public static void main(String[] args) {
-        Hashtable<HumanBeing,Car> collection =new Hashtable<>();
-        collection.put(new HumanBeing(),new Car());
+
     }
 }
