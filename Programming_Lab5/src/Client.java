fc52d28b5913a4ae41258504ef0c208a3ad8c380
HusseinHalifaev <HusseinHalifaev@github.com>
2020-02-29 14:09:01 +0300

    Fixed

--- Programming_Lab5/src/Client.java
+++ Programming_Lab5/src/Client.java
@@ -2,14 +2,20 @@
 import HumanBeings.HumanBeing;
 import HumanBeings.HumanReader;
 
+import java.util.Hashtable;
 import java.util.InputMismatchException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws InputMismatchException {
         Scanner sc = new Scanner(System.in);
-        HumanReader humanReader = new HumanReader();
-        HumanReader.readHuman(sc);
+        Hashtable<Integer, HumanBeing> collectionn = new Hashtable<>();
+        /*
+        Дописать условие при возращении HumanReader'ом null'а
+         */
+        collectionn.put(19, HumanReader.readHuman(sc));
+        System.out.println(collectionn.get(19));
 
     }
 }
--- Programming_Lab5/src/HumanBeings/HumanReader.java
+++ Programming_Lab5/src/HumanBeings/HumanReader.java
@@ -1,8 +1,11 @@
 package HumanBeings;
 import java.util.Scanner;
-public  class HumanReader {
-    public static boolean catchedError=false;
+public abstract   class HumanReader {
+    public static boolean catchedError;
+    public static boolean errorForCycle;
     public static HumanBeing readHuman(Scanner sc){
+        catchedError = false;
+        errorForCycle=false;
         String name = readerName("Введите имя", sc);
         Coordinates coordinates=readerCoordinates("Введите координаты(x,y):",sc);
         Boolean realHero = readerBoolean("Введите параметр realHero(True/False):",sc);
@@ -12,17 +15,20 @@ public  class HumanReader {
         WeaponType weaponType= readerWeaponType("Введите параметр weapon type(HAMMER/SHOTGUN/MACHINE_GUN):",sc);
         Car car = readerCar("Введите параметры name и cool(True/False) у Car:",sc);
         Mood mood = readerMood("Введите параметр Mood(GLOOM/RAGE/FRENZY):",sc);
-        if (catchedError=true)
+        if(errorForCycle){
+            return new HumanBeing(name, coordinates, realHero, hasToothpick, impactSpeed, minutesOfWaiting, weaponType, mood, car);
+        }else{
             return null;
-        return new HumanBeing(name,coordinates,realHero,hasToothpick,impactSpeed,minutesOfWaiting, weaponType, mood,car);
+        }
     }
-
     private static Mood readerMood(String request, Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         Mood mood;
         while(true){
             try {
                 mood=Mood.valueOf(sc.nextLine().trim());
+                errorForCycle=true;
                 break;
             }catch (Exception e){
                 System.out.println("Введите корректное значение ");
@@ -34,6 +40,7 @@ public  class HumanReader {
 
     private static Car readerCar(String request, Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         String name;
         boolean cool;
         while (true) {
@@ -45,10 +52,12 @@ public  class HumanReader {
             }else {
                 if (str[1].equals("True") || str[1].equals("true")) {
                     cool = true;
+                    errorForCycle=true;
                     break;
                 } else {
                     if (str[1].equals("False") || str[1].equals("false")) {
                         cool = false;
+                        errorForCycle=true;
                         break;
                     } else {
                         System.out.println("Введите корректное значение");
@@ -62,10 +71,12 @@ public  class HumanReader {
 
     private static WeaponType readerWeaponType(String request, Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         WeaponType weaponType;
         while (true){
             try {
                 weaponType=WeaponType.valueOf(sc.nextLine().trim());
+                errorForCycle=true;
                 break;
             }catch (Exception e){
                 System.out.println("Введите корректное значение");
@@ -78,10 +89,12 @@ public  class HumanReader {
 
     private static Float readerFloat(String request, Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         float minutesOfWaiting;
         while(true) {
             try {
                 minutesOfWaiting= Float.parseFloat(sc.nextLine());
+                errorForCycle=true;
                 break;
             }catch (Exception e){
                 System.out.println("Введите корректное значение ");
@@ -93,6 +106,7 @@ public  class HumanReader {
 
     private static Double readerDouble(String request,Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         double impactSpeed;
         while(true) {
             try {
@@ -100,6 +114,7 @@ public  class HumanReader {
                 if(impactSpeed<-680){
                     throw new Exception();
                 }
+                errorForCycle=true;
                 break;
             }catch (Exception e){
                 System.out.println("Введите корректное значение ");
@@ -111,15 +126,18 @@ public  class HumanReader {
 
     private static boolean readerBoolean(String request,Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         boolean trueOrFalse;
         while (true) {
             String[] str = sc.nextLine().split(" ");
             if(str[0].equals("True")||str[0].equals("true")){
                 trueOrFalse=true;
+                errorForCycle=true;
                 break;
             }else{
                 if (str[0].equals("False")||str[0].equals("false")){
                     trueOrFalse=false;
+                    errorForCycle=true;
                     break;
                 }else{
                     System.out.println("Введите корректное значение");
@@ -132,6 +150,7 @@ public  class HumanReader {
 
     private static Coordinates readerCoordinates(String request, Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         int x;
         double y;
         while(true) {
@@ -144,6 +163,7 @@ public  class HumanReader {
             try {
                 x = Integer.parseInt(line[0]);
                 y = Double.parseDouble(line[1]);
+                errorForCycle=true;
                 break;
             }catch (Exception e){
                 System.out.println("Введите корректное значение");
@@ -155,10 +175,12 @@ public  class HumanReader {
 
     public static String readerName(String request, Scanner sc){
         System.out.println(request);
+        errorForCycle=false;
         String name=sc.nextLine().trim();
         while(name.equals("")){
             System.out.println("Поле имя не может быть пустым.");
             name=sc.nextLine().trim();
+            errorForCycle=true;
             catchedError=true;
         }
         return name;
