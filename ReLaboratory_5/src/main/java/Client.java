b20589a4c51f180a9e2026cdf6df331d27e75d87
HusseinHalifaev <HusseinHalifaev@github.com>
2020-03-13 23:01:07 +0300

    I'm starting new laboratory, because -> Polina.

--- ReLaboratory_5/src/main/java/Client.java
+++ ReLaboratory_5/src/main/java/Client.java
@@ -1,5 +1,13 @@
+import ObjectSpecifications.HumanBeing;
+import ObjectSpecifications.HumanReader;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
-
+        Hashtable<Integer, HumanBeing> collection = new Hashtable<>();
+        Scanner sc = new Scanner(System.in);
+        collection.put(1,new HumanReader().readHuman(sc));
     }
 }
--- ReLaboratory_5/src/main/java/ObjectSpecifications/HumanReader.java
+++ ReLaboratory_5/src/main/java/ObjectSpecifications/HumanReader.java
@@ -8,15 +8,31 @@ public class HumanReader {
     public HumanBeing readHuman(Scanner sc) {
         catchedError = false;
         String name = readerName("Введите имя", sc);
-        Coordinates coordinates = readerCoordinates("Введите координаты(x,y):", sc);
+        Coordinates coordinates = readerCoordinates(sc);
         Boolean realHero = readerBoolean("Введите параметр realHero(True/False):", sc);
         Boolean hasToothpick = readerBoolean("Введите параметр has tooth pick(True/False)", sc);
         Double impactSpeed = readerDouble("Введите параметр impactSpeed(> -680):", sc);
         Float minutesOfWaiting = readerFloat("Введите параметр minutes of waiting:", sc);
         WeaponType weaponType = readerWeaponType("Введите параметр weapon type(HAMMER/SHOTGUN/MACHINE_GUN):", sc);
-        Car car = readerCar("Введите параметры name и cool(True/False) у ObjectSpecifications.Car:", sc);
-        Mood mood = readerMood("Введите параметр ObjectSpecifications.Mood(GLOOM/RAGE/FRENZY):", sc);
+        Car car = readerCar(sc);
+        Mood mood = readerMood("Введите параметр Mood(GLOOM/RAGE/FRENZY):", sc);
         return new HumanBeing(name, coordinates, realHero, hasToothpick, impactSpeed, minutesOfWaiting, weaponType, mood, car);
+    }
+
+    private Car readerCar(Scanner sc) {
+        String name;
+        boolean cool;
+        name=readerName("Введите значение name у Car:", sc);
+        cool=readerBoolean("Введите значение cool(True/False) у Car:",sc);
+        return new Car(name,cool);
+    }
+
+    private Coordinates readerCoordinates(Scanner sc) {
+        int x;
+        double y;
+        x=readerInteger("Введите координату x: ",sc);
+        y=readerDouble("Введите координату y: ",sc);
+        return new Coordinates(x, y);
     }
 
     private Mood readerMood(String request, Scanner sc) {
@@ -34,33 +50,6 @@ public class HumanReader {
         return mood;
     }
 
-    private Car readerCar(String request, Scanner sc) {
-        System.out.println(request);
-        String name;
-        boolean cool;
-        while (true) {
-            String[] str = sc.nextLine().toLowerCase().split(" ");
-            name = str[0];
-            if (str.length < 2) {
-                System.out.println("Введите корректное значение");
-                catchedError = true;
-            } else {
-                if (str[1].equals("true")) {
-                    cool = true;
-                    break;
-                } else {
-                    if (str[1].equals("false")) {
-                        cool = false;
-                        break;
-                    } else {
-                        System.out.println("Введите корректное значение");
-                        catchedError = true;
-                    }
-                }
-            }
-        }
-        return new Car(name, cool);
-    }
 
     private WeaponType readerWeaponType(String request, Scanner sc) {
         System.out.println(request);
@@ -95,11 +84,11 @@ public class HumanReader {
 
     private Double readerDouble(String request, Scanner sc) {
         System.out.println(request);
-        double impactSpeed;
+        double ScannedDouble;
         while (true) {
             try {
-                impactSpeed = Double.parseDouble(sc.nextLine());
-                if (impactSpeed <= -680) {
+                ScannedDouble = Double.parseDouble(sc.nextLine());
+                if(request.contains("impactSpeed") && ScannedDouble <= -680){
                     throw new Exception();
                 }
                 break;
@@ -108,7 +97,7 @@ public class HumanReader {
                 catchedError = true;
             }
         }
-        return impactSpeed;
+        return ScannedDouble;
     }
 
     private boolean readerBoolean(String request, Scanner sc) {
@@ -131,30 +120,20 @@ public class HumanReader {
         }
         return trueOrFalse;
     }
-
-    private Coordinates readerCoordinates(String request, Scanner sc) {
+    private int readerInteger(String request,Scanner sc){
         System.out.println(request);
-        int x;
-        double y;
+        int Int;
         while (true) {
-            String[] line = sc.nextLine().split(" ");
-            if (line.length < 2) {
-                System.out.println("Введите корректное значение");
-                catchedError = true;
-                continue;
-            }
             try {
-                x = Integer.parseInt(line[0]);
-                y = Double.parseDouble(line[1]);
+                Int = Integer.parseInt(sc.nextLine());
                 break;
             } catch (Exception e) {
                 System.out.println("Введите корректное значение");
                 catchedError = true;
             }
         }
-        return new Coordinates(x, y);
+        return Int;
     }
-
     private String readerName(String request, Scanner sc) {
         System.out.println(request);
         String name = sc.nextLine().trim();
