39e53764bc8fe8ea926e02228fa2a08cf85cd66f
HusseinHalifaev <HusseinHalifaev@github.com>
2020-06-12 18:10:05 +0300

    added OPD labs

--- Programming/Test3/src/ReadPrint/IMatrix.java
+++
@@ -1,8 +0,0 @@
-package ReadPrint;
-
-import java.util.Scanner;
-
-public interface IMatrix {
-    public void matrixReader(Scanner sc, int [][] matrixR);
-    public void printMatrix(int [][] matrix);
-}
